3ae83ab11882ed08b796f896e4ef5037d93739dc
Administrator <Administrator@PC-20181228BVOA>
2019-05-06 09:25:17 +0800

    2

--- wp/src/com/tedu/Add.java
+++ wp/src/com/tedu/Add.java
@@ -3,6 +3,6 @@ package com.tedu;
 public class Add {
 	public static void main(String[] args) {
 		System.out.println("helloworld!");
-		System.out.println("ÄãºÃ!");
+		System.out.println("ÍüÇéË®!");
 	}
 }
